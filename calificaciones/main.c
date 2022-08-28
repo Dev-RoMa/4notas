0ffe122b4a7b72d134641170db1f77799f06ff67
rodrigoMaureira <102636132+rodrigoMaureira@users.noreply.github.com>
2022-08-28 15:36:08 -0400

    Add files via upload

--- calificaciones/main.c
+++ calificaciones/main.c
@@ -14,28 +14,61 @@ int main()
     float notaf;
 
     printf("Bienvenido\n");
-    printf(" Ingrese primera nota = ");
+    printf("Ingrese primera nota = ");
     scanf("%f", &nota);
-    print(\n);
+    printf("\n");
     printf("%.2f", nota);
+    printf("\n");
 
-    printf(" Ingrese segunda nota = ");
+    printf("Ingrese segunda nota = ");
     scanf("%f", &nota1);
-    print(\n);
+    printf("\n");
     printf("%.2f", nota1);
+    printf("\n");
 
-    printf(" Ingrese tercera nota = ");
+    printf("Ingrese tercera nota = ");
     scanf("%f", &nota2);
-    print(\n);
+    printf("\n");
     printf("%.2f", nota2);
+    printf("\n");
 
-    printf(" Ingrese cuarta nota = ");
+    printf("Ingrese cuarta nota = ");
     scanf("%f", &nota3);
-    print(\n);
+    printf("\n");
     printf("%.2f", nota3);
+    printf("\n");
 
-    printf(" su promedio se calculara con las cuatro notas \n");
+    printf("su promedio se calculara con las cuatro notas \n");
     notaf = nota + nota1+ nota2 + nota3;
     notaf = notaf/4;
-    printf("su nota final es = %.2f", notaf);
+    printf("su nota final es = %.2f\n", notaf);
+
+        /* si es menor a 5 ha reprobado
+    si es 5 es suficiente, 6 es bien,
+    7 y 8 es notable; sobresaliente 9-10*/
+
+    if (notaf >= 5.0 && notaf < 6.0) {
+        printf("ha aprobado con suficiente");
+    }
+        else if (notaf < 1.0) {
+            printf("no puede tener promedio menor a 5.0 !!!\nreintente");
+        }
+        else if (notaf >= 6.0 && notaf < 7.0){
+            printf("ha aprobado bien ");
+        }
+        else if (notaf >= 7.0 && notaf < 9.0){
+            printf("ha aprobado de manera notable ");
+        }
+        else if (notaf >= 9.0 && notaf < 10.0) {
+            printf("ha aprobado de manera sobresaliente\n felicitaciones !!!");
+        }
+        else if (notaf > 10.0) {
+            printf("no puede tener un promedio sobre 10.0 -_-\nreintente");
+        }
+    else {
+        printf("\nha reprobado ");
+    }
+    printf("\n---------------------------------------\n");
+    printf("fin del programa");
+    printf("\n---------------------------------------\n");
 }
